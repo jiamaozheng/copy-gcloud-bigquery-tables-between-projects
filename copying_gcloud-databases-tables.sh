a6b383b82674bbfcd022a220af3bbe1bc76c8e02
Jiamao Zheng <jiamaozheng@nsit-dhcp-128-135-76-124.uchicago.edu>
2018-03-07 12:55:24 -0600

    init

---
+++ copying_gcloud-databases-tables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Modified from https://stackoverflow.com/questions/32724767/any-feature-in-bigquery-that-can-migrate-a-whole-dataset-in-another-project-w-o
+
+export SOURCE_DATASET=$1  # project1:dataset
+export DEST_PREFIX=$2  # project2:dataset2.any_prefix_
+for f in `bq ls $SOURCE_DATASET |grep TABLE | awk '{print $1}'`
+do
+  export CP_COMMAND="bq cp $SOURCE_DATASET.$f $DEST_PREFIX$f"
+  echo $CP_COMMAND
+  echo `$CP_COMMAND`
+done
---
+++ run.copying_gcloud-databases-tables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sh copying_gcloud-databases-tables.sh predictdb-187320:GTEx_V7_HapMap_2017_11_29 gtex-awg-im:GTEx_V7_HapMap_2017_11_29.
+sh copying_gcloud-databases-tables.sh predictdb-187320:GTEx_V7_all_HapMap_2017_11_29 gtex-awg-im:GTEx_V7_all_HapMap_2017_11_29.
+sh copying_gcloud-databases-tables.sh predictdb-187320:GTEx_V6p_New_HapMap_2017_11_29 gtex-awg-im:GTEx_V6p_New_HapMap_2017_11_29.
+sh copying_gcloud-databases-tables.sh predictdb-187320:GTEx_V6p_HapMap_2016_09_08 gtex-awg-im:GTEx_V6p_HapMap_2016_09_08.
+sh copying_gcloud-databases-tables.sh predictdb-187320:GTEx_V6p_1KG_2016_11_16 gtex-awg-im:GTEx_V6p_1KG_2016_11_16.
+sh copying_gcloud-databases-tables.sh predictdb-187320:DGN_HapMap_2015 gtex-awg-im:DGN_HapMap_2015.
+sh copying_gcloud-databases-tables.sh gen-arch:genarch gtex-awg-im:genarch.
