65cfb563d474529740ee6070697a2d1323043fd7
Jiamao Zheng <jiamaozheng@Jiamaos-MacBook-Pro.local>
2018-05-09 11:22:18 -0500

    test

--- copying_gcloud-databases-tables.sh
+++ copying_gcloud-databases-tables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# author: Jiamao Zheng 
 # Modified and modified from https://stackoverflow.com/questions/32724767/any-feature-in-bigquery-that-can-migrate-a-whole-dataset-in-another-project-w-o
 
 export SOURCE_DATASET=$1  # project1:dataset
